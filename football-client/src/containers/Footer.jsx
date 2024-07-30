2bbcb1f8dd41eea7e0ea8094974db527904fe7be
Joel Ismar Florentin <j.ismar00@gmail.com>
2024-07-29 20:00:30 -0400

    Modify navbar info, and add footer

--- football-client/src/containers/Footer.jsx
+++ football-client/src/containers/Footer.jsx
@@ -1,5 +1,70 @@
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <footer className="bg-gray-100">
+            <div className="mx-auto max-w-5xl px-4 py-16 sm:px-6 lg:px-8">
+                <div className="flex justify-center text-teal-600">
+                    <svg xmlns="http://www.w3.org/2000/svg" color="rgba(13, 148, 136)" width={50} height={45} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-play-football">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M11 4a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
+                        <path d="M3 17l5 1l.75 -1.5" />
+                        <path d="M14 21v-4l-4 -3l1 -6" />
+                        <path d="M6 12v-3l5 -1l3 3l3 1" />
+                        <path d="M19.5 20a.5 .5 0 1 0 0 -1a.5 .5 0 0 0 0 1z" fill="currentColor" />
+                    </svg>
+                </div>
+
+                <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500">
+                    “This is a web project that focuses on the football match results of some teams, built using JavaScript, Fastify, React, and PostgreSQL.” 🌐⚽
+                </p>
+
+                <ul className="mt-12 flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
+                    <li>
+                        <a className="text-gray-700 transition hover:text-gray-700/75" href="https://j0t4ku.github.io/"> About me </a>
+                    </li>
+
+                    <li>
+                        <a className="text-gray-700 transition hover:text-gray-700/75" href="https://github.com/j0t4ku/footbool-mager"> This Projects </a>
+                    </li>
+
+                </ul>
+
+                <ul className="mt-12 flex justify-center gap-6 md:gap-8">
+                    <li>
+                        <a
+                            href="https://x.com/j0t4ku_"
+                            rel="noreferrer"
+                            target="_blank"
+                            className="text-gray-700 transition hover:text-gray-700/75"
+                        >
+                            <span className="sr-only">Twitter</span>
+                            <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                <path
+                                    d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
+                                />
+                            </svg>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a
+                            href="https://github.com/j0t4ku"
+                            rel="noreferrer"
+                            target="_blank"
+                            className="text-gray-700 transition hover:text-gray-700/75"
+                        >
+                            <span className="sr-only">GitHub</span>
+                            <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                <path
+                                    fillRule="evenodd"
+                                    d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+        </footer>
     )
 }
--- football-client/src/containers/Navbar.jsx
+++ football-client/src/containers/Navbar.jsx
@@ -21,27 +21,11 @@ export default function Navbar() {
                         <nav aria-label="Global" className="hidden md:block">
                             <ul className="flex items-center gap-6 text-sm">
                                 <li>
-                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> About </a>
+                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="https://j0t4ku.github.io/"> About Me</a>
                                 </li>
 
                                 <li>
-                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Careers </a>
-                                </li>
-
-                                <li>
-                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> History </a>
-                                </li>
-
-                                <li>
-                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Services </a>
-                                </li>
-
-                                <li>
-                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Projects </a>
-                                </li>
-
-                                <li>
-                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Blog </a>
+                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="https://github.com/j0t4ku/footbool-mager"> This Repository </a>
                                 </li>
                             </ul>
                         </nav>
